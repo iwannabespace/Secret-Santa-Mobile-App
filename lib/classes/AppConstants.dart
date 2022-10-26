68295f10cec800ee67fcddef27688d7869c5d764
iwannabespace <thertu58@gmail.com>
2022-10-26 19:13:06 +0300

    Added AppConstants

---
+++ lib/classes/AppConstants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const Color primaryColor = Color.fromARGB(255, 255, 246, 247);
+  static const Color secondaryColor = Colors.white;
+}
