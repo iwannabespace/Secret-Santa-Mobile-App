59a8439a0dcfe03033f0e3c800699feff069f45d
iwannabespace <thertu58@gmail.com>
2022-10-28 22:12:29 +0300

    sad

--- lib/screens/gift_screen.dart
+++ lib/screens/gift_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:project/classes/AppConstants.dart';
+import '../classes/card_page_custom_appbar.dart';
 
 class GiftScreen extends StatefulWidget {
   const GiftScreen({Key? key}) : super(key: key);
@@ -11,20 +13,8 @@ class _GiftScreenState extends State<GiftScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          InkWell(
-            onTap: () {
-              Navigator.of(context).pop();
-            },
-            child: Container(
-              child: Center(child: Icon(Icons.arrow_back)),
-            ),
-          ),
-        ],
-      ),
+      appBar: CustomAppBar(),
+      backgroundColor: AppConstants.secondaryColor,
     );
   }
 }
